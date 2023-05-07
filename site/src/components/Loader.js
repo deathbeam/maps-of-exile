7fe6d9fd0ce61e0e8c49a5d250bef110b549f7c5
Tomas Slusny <slusnucky@gmail.com>
2023-05-07 04:31:15 +0200

    Fix classname for loader
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/src/components/Loader.js
+++ site/src/components/Loader.js
@@ -2,7 +2,7 @@ import './Loader.css'
 
 const Loader = ({ loading }) => (
   <div
-    class="fixed-top animated loader"
+    className="fixed-top animated loader"
     style={{ display: loading ? 'block' : 'none' }}
   />
 )

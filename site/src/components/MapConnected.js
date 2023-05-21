c4280bf03d35fb9bc246aab3e1f6b786d6bc35d8
Tomas Slusny <slusnucky@gmail.com>
2023-05-21 03:08:53 +0200

    Fix connected map scrolling
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/src/components/MapConnected.js
+++ site/src/components/MapConnected.js
@@ -3,7 +3,7 @@ import { scrollToElement } from '../common'
 const MapConnected = ({ connected }) => {
   return connected.map(m => (
     <>
-      <button className="btn text-dark btn-secondary m-1" onClick={() => scrollToElement(m)}>
+      <button className="btn text-dark btn-secondary m-1" onClick={() => scrollToElement(m.name)}>
         <b>{Math.round(m.score) + ' '}</b>
         {m.name}
       </button>

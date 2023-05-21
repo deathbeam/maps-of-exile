6766ab5146d50b16141b78d28785796ee47fe052
Tomas Slusny <slusnucky@gmail.com>
2023-05-21 05:02:27 +0200

    Improve display of connected maps in list
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/src/components/MapConnected.js
+++ site/src/components/MapConnected.js
@@ -1,10 +1,10 @@
-import { scrollToElement } from '../common'
+import { ratingColor, scrollToElement } from '../common'
 
 const MapConnected = ({ connected }) => {
   return connected.map(m => (
     <>
-      <button className="btn text-dark btn-secondary m-1" onClick={() => scrollToElement(m.name)}>
-        <b>{Math.round(m.score) + ' '}</b>
+      <button className="btn btn-dark m-1" onClick={() => scrollToElement(m.name)}>
+        <b className={`text-${ratingColor(m.score, 10)}`}>{Math.round(m.score) + ' '}</b>
         {m.name}
       </button>
       <br />

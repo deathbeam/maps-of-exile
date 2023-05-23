1db799073c5c569920b769850d391ef91701aee7
Tomas Slusny <slusnucky@gmail.com>
2023-05-23 03:44:16 +0200

    Show at least map name when scrolling
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/src/components/Map.js
+++ site/src/components/Map.js
@@ -17,17 +17,17 @@ const Map = ({ map, currentSearch, addToInput }) => {
       ref={ref}
       className="map-image lazy-bg"
     >
-      {visible && (
+      <td>
+        <MapName map={map} currentSearch={currentSearch} addToInput={addToInput} />
+        <div className="d-md-none mt-2">
+          <Rating rating={map.score} scale={10} label="Total" />
+          <Rating rating={map.rating.layout} label="Layout" />
+          <Rating rating={map.rating.density} label="Density" />
+          <Rating rating={map.rating.boss} label="Boss" />
+        </div>
+      </td>
+      {visible ? (
         <>
-          <td>
-            <MapName map={map} currentSearch={currentSearch} addToInput={addToInput} />
-            <div className="d-md-none mt-2">
-              <Rating rating={map.score} scale={10} label="Total" />
-              <Rating rating={map.rating.layout} label="Layout" />
-              <Rating rating={map.rating.density} label="Density" />
-              <Rating rating={map.rating.boss} label="Boss" />
-            </div>
-          </td>
           <td className="text-center d-none d-md-table-cell">
             <Rating rating={map.rating.layout} tooltip={map.info.layout} />
           </td>
@@ -43,6 +43,14 @@ const Map = ({ map, currentSearch, addToInput }) => {
           <td>
             <MapCards cards={map.cards} />
           </td>
+        </>
+      ) : (
+        <>
+          <td />
+          <td />
+          <td />
+          <td />
+          <td />
         </>
       )}
     </tr>

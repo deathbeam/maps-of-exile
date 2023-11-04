a0f46bb7a962af5b8c64c0de99414a9f54087e80
Tomas Slusny <slusnucky@gmail.com>
2023-11-04 03:27:44 +0100

    Properly check for target
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/src/hooks/usePersistedState.js
+++ site/src/hooks/usePersistedState.js
@@ -44,7 +44,7 @@ export default function usePersistedState(key, def, startTransition, locationRef
   return [
     val,
     e => {
-      const val = Object.hasOwn(e, 'target') ? e.target.value : e
+      const val = e && e.target ? e.target.value : e
       if (startTransition) {
         startTransition(() => setVal(val === '' ? def : parseValue(val, def)))
       } else {

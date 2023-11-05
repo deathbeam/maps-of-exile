d3e52b94d2f2c129ccc38689670df19ef95b4031
Tomas Slusny <slusnucky@gmail.com>
2023-11-06 00:19:32 +0100

    Run lint on CI
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/.eslintrc.cjs
+++ site/.eslintrc.cjs
@@ -13,9 +13,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/prop-types': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off'
   },
 }

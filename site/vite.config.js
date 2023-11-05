6d37f7659cfb884556862eb2b69bb29f145a5965
Tomas Slusny <slusnucky@gmail.com>
2023-11-05 20:02:21 +0100

    Try to switch to preact
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
+import { preact } from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
   build: { target: browserslistToEsbuild() }
 })

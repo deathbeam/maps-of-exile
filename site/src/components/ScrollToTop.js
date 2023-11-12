80f58352a8fa5ae3846a26c05ce3658f4c2584c0
Tomas Slusny <slusnucky@gmail.com>
2023-11-12 13:13:31 +0100

    Fix ScrollToTop
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- site/src/components/ScrollToTop.js
+++ site/src/components/ScrollToTop.js
@@ -1,9 +1,14 @@
+import { useAtomValue } from 'jotai'
 import { useEffect } from 'react'
 
+import state from '../state'
+
 const ScrollToTop = () => {
+  const location = useAtomValue(state.location)
+
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [])
+  }, [location])
 
   return null
 }
